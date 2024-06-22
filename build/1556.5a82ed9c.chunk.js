923b4ecc5bdbbe73cdccab010aa346bb6c57a484
Ayush Pandey <baku@Bakus-Mac.local>
2024-06-23 01:30:12 +0545

    this is backend

---
+++ build/1556.5a82ed9c.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdoctor_appointment_admin=self.webpackChunkdoctor_appointment_admin||[]).push([[1556],{41556:(e,n,d)=>{d.r(n),d.d(n,{default:()=>t});const t={}}}]);
---
+++ build/8366.2f14dd3f.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdoctor_appointment_admin=self.webpackChunkdoctor_appointment_admin||[]).push([[8366],{48366:(a,n,t)=>{t.r(n),t.d(n,{default:()=>d});const d={}}}]);
